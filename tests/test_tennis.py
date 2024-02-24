055ed504e66f8bda8ceee2f9158a444ca02d2dad
ketannabera <ketannabera8@gmail.com>
2024-02-24 08:24:43 +0530

    Feat-1 | Initital code

--- tests/test_tennis.py
+++ tests/test_tennis.py
@@ -2,8 +2,7 @@
 
 import pytest
 
-from src.tennis_game_1 import TennisGame1, TennisGame3
-from src.tennis_game_2 import TennisGame2
+from src.tennis_game_1 import TennisGame1
 from tests.conftest import test_cases
 
 pytestmark = pytest.mark.tennis
@@ -14,15 +13,6 @@ def test_get_score_game1(p1Points, p2Points, score, p1Name, p2Name):
     game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
     assert score == game.score()
 
-@pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
-def test_get_score_game2(p1Points, p2Points, score, p1Name, p2Name):
-    game = play_game(TennisGame2, p1Points, p2Points, p1Name, p2Name)
-    assert score == game.score()
-
-@pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
-def test_get_score_game3(p1Points, p2Points, score, p1Name, p2Name):
-    game = play_game(TennisGame3, p1Points, p2Points, p1Name, p2Name)
-    assert score == game.score()
 
 def play_game(TennisGame, p1Points, p2Points, p1Name, p2Name):
     game = TennisGame(p1Name, p2Name)
@@ -32,24 +22,3 @@ def play_game(TennisGame, p1Points, p2Points, p1Name, p2Name):
         if i < p2Points:
             game.won_point(p2Name)
     return game
-
-#
-
-#
-# def test_Score_Game1():
-#     for testcase in test_cases:
-#         (p1Points, p2Points, score, p1Name, p2Name) = testcase
-#         game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
-#         assert score == game.score()
-#
-# def test_Score_Game2():
-#     for testcase in test_cases:
-#         (p1Points, p2Points, score, p1Name, p2Name) = testcase
-#         game = play_game(TennisGame2, p1Points, p2Points, p1Name, p2Name)
-#         assert score == game.score()
-#
-# def test_Score_Game3():
-#     for testcase in test_cases:
-#         (p1Points, p2Points, score, p1Name, p2Name) = testcase
-#         game = play_game(TennisGame3, p1Points, p2Points, p1Name, p2Name)
-#         assert score == game.score()
